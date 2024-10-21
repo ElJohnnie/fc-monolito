44382d019d03decb0b4a123cb8858dfde640d733
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-10-21 10:12:57 -0300

    finish checkout

--- src/modules/invoices/factory/invoice.facade.factory.ts
+++ src/modules/invoices/factory/invoice.facade.factory.ts
@@ -1,7 +1,5 @@
-import UseCaseInterface from "../../@shared/usecase/use-case.interface";
 import InvoiceFacade from "../facade/invoice.facade";
 import InvoiceFacadeInterface from "../facade/invoice.facade.interface";
-import InvoiceGateway from "../gateway/invoice.gateway";
 import InvoiceRepository from "../repository/invoice.repository";
 import FindInvoiceUseCase from "../usecase/find/find.usecase";
 import GenerateInvoiceUseCase from "../usecase/generate/generate.usecase";
@@ -13,7 +11,6 @@ export default class InvoiceFacadeFactory {
     const findInvoiceUseCase = new FindInvoiceUseCase(repository);
 
     return new InvoiceFacade(
-      repository,
       generateInvoiceUseCase,
       findInvoiceUseCase
     );
